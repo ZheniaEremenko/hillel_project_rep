ea083ae3786b01691be14d2b048a3479ab3bd8ca
zhenia <zheniaeremenko@gmail.com>
2023-12-04 17:15:19 +0200

    5lessons Homework

---
+++ first_project_hillel/ReversArrayProject_5lessons.js
@@ -0,0 +1,52 @@
+const name = 'My worb';
+let EmtyString = ' ';
+let PolidromName = 'Aba';
+let NotPolidromName = '1234';
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
+
+function number_of_characters(name) {
+    for (let i = name.length; i >= 0; i--) {
+        return i;
+    }
+
+};
+
+function revers_worb() {
+    for (let i = name.length - 1; i >= 0; i--)
+        EmtyString += name[i];
+    return console.log(`My default verb is' ${name} ', But my revers worb is ' ${EmtyString} '`)
+
+};
+
+function palindrome(PolidromName) {
+    let opt = /[^A-Za-z0-9]/g;
+    let lowerCaseStr = PolidromName.toLowerCase().replace(opt, '');
+    let reverse_worb = lowerCaseStr.split('').reverse().join('');
+    return reverse_worb === lowerCaseStr;
+}
+
+
+function paired_numbers_from_array(array) {
+    let emtyArray = [' '];
+    for (let i = 0; i < array.length; i++) {
+
+
+        if ((array[i] % 2) === 0) {
+
+            for (let i = array.length - 2; i >= 0; i--)
+                emtyArray += array[i];
+
+            console.log(array[i]);
+        }
+    }
+}
+
+
+
+console.log(number_of_characters(name))
+console.log(revers_worb(name))
+console.log(`================================ \n Polidrome: ${PolidromName}, NotPolidrom: ${NotPolidromName} \n================================`)
+console.log(palindrome(PolidromName))
+console.log(palindrome(NotPolidromName))
+console.log(paired_numbers_from_array(array))
